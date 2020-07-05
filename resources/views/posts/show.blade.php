103a7ccde3d68395fb336cd94a5323d1687dbf41
Ghanashyam Kamath <ghanashyam.kamath99@gmail.com>
2020-07-05 12:16:55 +0530

    delete feature corrected

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,9 +15,11 @@
     <hr>
     {{-- edit  --}}
     <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-    {{!!Form::open(['action' => ['PostsController@destroy',$post->id],'method'=> 'POST','class'=>'pull-right'])!!}}
+    <br><br>
+    
+    {!!Form::open(['action' => ['PostsController@destroy',$post->id],'method'=> 'POST','class'=>'pull-right'])!!}
     {{Form::hidden('_method','DELETE')}}
     {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
-    {{!!Form::close()!!}}
+    {!!Form::close()!!}
 
 @endsection
